fa6982e36d86e5d16ae9df974c5566f5846d1c4c
APO <ringo1625@gmail.com>
2015-07-10 13:41:39 +0900

    VisualStudio2013Web
    実際のプログラム

---
+++ 2015071012_web/MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        // Get: Send
+        [HttpPost]
+        public ActionResult Send(string value1, string value2)
+        {
+              ViewData["PostData"] = value1 + "+" + value2 + "=" + (int.Parse(value1)+int.Parse(value2)).ToString();
+            return View();
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
